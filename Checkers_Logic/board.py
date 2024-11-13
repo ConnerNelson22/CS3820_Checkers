7beaf77d577f7d85718e4d7169676a28eaaea4ed
schafe1 <schafe@uccs.edu>
2024-11-13 14:59:30 -0700

    Update board.py

--- Checkers_Logic/board.py
+++ Checkers_Logic/board.py
@@ -43,3 +43,42 @@ class Board:
             
             #Combing the piece difference and plus the king difference
             return piece_count_diff + king_piece_diff
+
+        def get_all_pieces(self, color):
+        #Initialzing an empty list
+        pieces = []
+
+        # Iterates over each row that contains multiple piece element
+        for row in self.board:
+             # Each piece in that row
+            for piece in row:
+                # If the piece is not empty then that piece represents that color
+                if piece != 0 and piece.color == color:
+                     # When condition is met, piece will be added to the pieces list
+                    pieces.append(piece)
+        return pieces
+
+        
+        def move(self, piece, row, col):
+
+        #Swap Positions on the board
+        #self.board[piece.row][piece.col] current position on the board
+        # self.board[row][col] is the target position
+        # then swap back
+        self.board[piece.row][piece.col], self.board[row][col] = self.board[row][col], self.board[piece.row][piece.col]
+        #Updating the pieces location
+        piece.move(row, col)
+
+        #Update kings count by color
+        #Check for promotion to king
+        # if the piece is the (ROWS - 1) last row or row == 0 is the opposite
+        if row == ROWS - 1 or row == 0:
+             #Then make the piece a king
+            piece.make_king()
+            # If the color piece is white
+            if piece.color == WHITE:
+                #Then that white king piece increments by 1
+                self.white_kings += 1
+             # else the red king is incremennt by 1
+            else:
+                self.red_kings += 1 
