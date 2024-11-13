450401fa650dfc3de3c1adf00915e2a26f3b4d97
ConnerNelson22 <143366648+ConnerNelson22@users.noreply.github.com>
2024-11-13 15:23:58 -0700

    adjusting board

--- Checkers_Logic/board.py
+++ Checkers_Logic/board.py
@@ -1,105 +1,60 @@
 import pygame
-
-#Using .constants to make sure that everything is constant
-from .constants import ROWS, COLS, SQUARE_SIZE, WHITE, BLACK, RED
+from .constants import BLACK, ROWS, RED, SQUARE_SIZE, COLS, WHITE
 from .piece import Piece
 
 class Board:
-        def __init(self):
-            #The [] means that the board will be initialized as an empty list
-            self.board = []
-            
-            #Both white and red pieces start out with 12
-            self.white_left = 12 
-            self.red_left = 12
-            
-            #Beginning of game, there are no kings
-            self.white_king = 0
-            self.red_king = 0
-            
-            #Calls method to set up the board
-            self.creating_board()
+    def __init__(self):
+        self.board = []
+        self.red_left = self.white_left = 12
+        self.red_kings = self.white_kings = 0
+        self.create_board()
+    
+    def draw_squares(self, win):
+        win.fill(BLACK)
+        for row in range(ROWS):
+            for col in range(row % 2, COLS, 2):
+                pygame.draw.rect(win, RED, (row*SQUARE_SIZE, col *SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
 
-        #Function rendering the checkers board on the will from using Pygame and filling it wither alternating colored square
-        def drawing_squares(self,win):
-            #win is windows and fill the entire window with black
-            win.fill(BLACK)
-            
-            #Alternating colored squares
-            for row in range(ROWS):
-                for col in range(0 if row % 2 == 0 else 1, COLS, 2):
-                  
-                    #Positioning for the squares ar switched
-                    position_col = col * SQUARE_SIZE
-                    position_row = row * SQUARE_SIZE
-                    pygame.draw.rect(win, RED, (position_col, position_row, SQUARE_SIZE, SQUARE_SIZE))
-            
-        def eveluating(self):
-            #Evaluting the difference in the pieces
-            piece_count_diff = self.white_left - self.red_left
-            
-            #Evaluating the difference in kings
-            king_piece_diff = 0.5 * (self.white_kings - self.red_kings)
-            
-            #Combing the piece difference and plus the king difference
-            return piece_count_diff + king_piece_diff
+    def evaluate(self):
+        return self.white_left - self.red_left + (self.white_kings * 0.5 - self.red_kings * 0.5)
 
-        def get_all_pieces(self, color):
-                #Initialzing an empty list
-                pieces = []
+    def get_all_pieces(self, color):
+        pieces = []
+        for row in self.board:
+            for piece in row:
+                if piece != 0 and piece.color == color:
+                    pieces.append(piece)
+        return pieces
 
-                # Iterates over each row that contains multiple piece element
-                for row in self.board:
-                     # Each piece in that row
-                    for piece in row:
-                     # If the piece is not empty then that piece represents that color
-                        if piece != 0 and piece.color == color:
-                             # When condition is met, piece will be added to the pieces list
-                            pieces.append(piece)
-                return pieces
+    def move(self, piece, row, col):
+        self.board[piece.row][piece.col], self.board[row][col] = self.board[row][col], self.board[piece.row][piece.col]
+        piece.move(row, col)
 
-        def move(self, piece, row, col):
-                #Swap Positions on the board
-                #self.board[piece.row][piece.col] current position on the board
-                # self.board[row][col] is the target position
-                # then swap back
-                self.board[piece.row][piece.col], self.board[row][col] = self.board[row][col], self.board[piece.row][piece.col]
-                #Updating the pieces location
-                piece.move(row, col)
+        if row == ROWS - 1 or row == 0:
+            piece.make_king()
+            if piece.color == WHITE:
+                self.white_kings += 1
+            else:
+                self.red_kings += 1 
 
-                #Update kings count by color
-                #Check for promotion to king
-                # if the piece is the (ROWS - 1) last row or row == 0 is the opposite
-                if row == ROWS - 1 or row == 0:
-                     #Then make the piece a king
-                    piece.make_king()
-                    # If the color piece is white
-                    if piece.color == WHITE:
-                        #Then that white king piece increments by 1
-                        self.white_kings += 1
-                             # else the red king is increment by 1
+    def get_piece(self, row, col):
+        return self.board[row][col]
+
+    def create_board(self):
+        for row in range(ROWS):
+            self.board.append([])
+            for col in range(COLS):
+                if col % 2 == ((row +  1) % 2):
+                    if row < 3:
+                        self.board[row].append(Piece(row, col, WHITE))
+                    elif row > 4:
+                        self.board[row].append(Piece(row, col, RED))
                     else:
-                        self.red_kings += 1 
-
-        #Getting the pieces and location of the 
-        def get_pieces(self, row, col):
-                return self.board[row][col]
-
-        #Function for creating the board
-        def create_board(self):
-                for row in range(ROWS):
-                    self.board.append([])
-                    for col in range(COLS):
-                        if col % 2 == ((row +  1) % 2):
-                                if row < 3:
-                                        self.board[row].append(Piece(row, col, WHITE))
-                                elif row > 4:
-                                        self.board[row].append(Piece(row, col, RED))
-                                else:
-                                        self.board[row].append(0)
-                        else:
-                            self.board[row].append(0)
-        def draw(self, win):
+                        self.board[row].append(0)
+                else:
+                    self.board[row].append(0)
+        
+    def draw(self, win):
         self.draw_squares(win)
         for row in range(ROWS):
             for col in range(COLS):
@@ -167,7 +122,9 @@ class Board:
                 break
             else:
                 last = [current]
+
             left -= 1
+        
         return moves
 
     def _traverse_right(self, start, stop, step, color, right, skipped=[]):
@@ -176,6 +133,7 @@ class Board:
         for r in range(start, stop, step):
             if right >= COLS:
                 break
+            
             current = self.board[r][right]
             if current == 0:
                 if skipped and not last:
